6110d0a6dbbe5c4e49296f6212cc3a833578a061
MariiaKlova <mauriell11@gmail.com>
2022-06-08 13:40:57 +0300

    36less

--- lessons/36/assets/js/dist/main.dev.js
+++ lessons/36/assets/js/dist/main.dev.js
@@ -8,4 +8,25 @@ $(document).ready(function () {
     infinite: true,
     dots: true
   });
+  $('.article_slider').slick({
+    slidesToShow: 3,
+    slidesToSckrol: 1,
+    adaptiveHeight: true,
+    infinite: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }, {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }]
+  });
 });
