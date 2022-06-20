5ea2e238f38b1c5e917ae1d6e0298532b9ac988f
MariiaKlova <mauriell11@gmail.com>
2022-06-20 18:55:08 +0300

    43les

---
+++ lessons/43gulp/gulpfile.js
@@ -0,0 +1,21 @@
+const { dest } = require('gulp');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp
+        .watch("src/*.html")
+        .on('change', browserSync.reload);
+});
+
+gulp.task('move-html', function () {
+    return gulp
+        .src('src/*.html')
+        .pipe(gulp.dest('dist/'));
+});
